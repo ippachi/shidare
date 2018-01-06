06875963e12e2b0c764736277068e6f8f1cd6464
ippachi <ivalice1218@gmail.com>
2018-01-06 17:37:24 +0900

    first commit

---
+++ lib/shidare.rb
@@ -0,0 +1,5 @@
+require "shidare/version"
+
+module Shidare
+  # Your code goes here...
+end
---
+++ spec/shidare_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Shidare do
+  it "has a version number" do
+    expect(Shidare::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
